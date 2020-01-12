45817f3e62660c14ea85be0934f4caa068e7bf66
Benjamin Slater <ben@hackcapital.com>
2020-01-11 17:36:20 -0800

    added index create base

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
 use reqwest;
+use reqwest::StatusCode;
 use tokio::runtime::Runtime;
 use serde::Deserialize;
 
@@ -24,21 +25,76 @@ struct Version {
     minimum_index_compatibility_version: String,
 }
 
-async fn es_info_req(client: reqwest::Client) -> reqwest::Result<String> {
+#[derive(Debug)]
+enum EsIndexCreate {
+    Success(EsIndexCreateSuccess),
+    Fail(EsIndexCreateFail),
+}
+
+#[derive(Deserialize, Debug)]
+struct EsIndexCreateSuccess {
+    acknowledged: bool,
+    shards_acknowledged: bool,
+    index: String,
+}
+
+#[derive(Deserialize, Debug)]
+struct EsIndexCreateFail {
+    error: EsIndexError,
+    status: u16,
+}
+
+#[derive(Deserialize, Debug)]
+struct EsIndexError {
+    root_cause: Vec<IndexCreateFailMetadata>,
+}
+
+#[derive(Deserialize, Debug)]
+struct IndexCreateFailMetadata {
+    #[serde(rename = "type")]
+    error_type: String,
+    reason: String,
+    index_uuid: String,
+    index: String,
+}
+
+async fn es_info_req(client: &reqwest::Client) -> reqwest::Result<EsInfo> {
     let res = client.get("http://localhost:9200")
         .send()
         .await?
-        .text()
+        .json::<EsInfo>()
         .await?;
     Ok(res)
 }
 
-async fn search_req(client: reqwest::Client) -> reqwest::Result<String> {
+async fn search_req(client: &reqwest::Client) -> reqwest::Result<String> {
     let res = client.post("http://localhost:9200")
         .send()
-        .await?
-        .text()
         .await?;
+
+    println!("{}", res.status());
+    Ok("test".to_owned())
+}
+
+
+async fn create_index_req(client: &reqwest::Client, index: &str) -> Result<EsIndexCreate, Box<dyn std::error::Error>> {
+    let res = client.put(&format!("{}/{}", "http://localhost:9200", index))
+        .send()
+        .await?;
+
+    let res = match res.status() {
+        StatusCode::OK => {
+            let text = res.text().await?;
+            let data = serialize_response::<EsIndexCreateSuccess>(&text)?;
+            EsIndexCreate::Success(data)
+        },
+        StatusCode::BAD_REQUEST => { 
+            let text = res.text().await?;
+            let data = serialize_response::<EsIndexCreateFail>(&text)?;
+            EsIndexCreate::Fail(data)
+        },
+        _ => panic!("Request failed in an unexpected way..."),
+    };
     Ok(res)
 }
 
@@ -50,11 +106,20 @@ fn serialize_response<T>(raw_str: &str) -> serde_json::Result<T>
 }
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
+    // Setup client and runtime.
     let mut rt = Runtime::new().unwrap();
     let client = reqwest::Client::new();
-    let future = es_info_req(client);
-    let raw_string = rt.block_on(future)?;
-    let info = serialize_response::<EsInfo>(&raw_string)?;
+
+    // Print info on cluster.
+    let info_future = es_info_req(&client);
+    let info = rt.block_on(info_future)?;
+    //let info = serialize_response::<EsInfo>(&raw_string)?;
     println!("{:?}", info);
+
+    // Create index.
+    let index_future = create_index_req(&client, "test3");
+    let index = rt.block_on(index_future)?;
+    println!("{:?}", index);
+
     Ok(())
 }
