ad1f5222ab31a3b882c3bc8b4bd2b3bbf94ffd82
Benjamin Slater <ben@hackcapital.com>
2020-01-25 20:06:15 -0800

    corrected test

--- src/index.rs
+++ src/index.rs
@@ -1,13 +1,12 @@
 use reqwest::StatusCode;
 use serde::Deserialize;
 use std::fmt;
-use tokio::runtime::Runtime;
 
 use crate::client::EsClient;
 use crate::utils::serialize_response;
 use crate::errors::ESClientCreateIndexFail;
 
-#[derive(Deserialize, Debug)]
+#[derive(Deserialize, Debug, PartialEq)]
 pub struct EsIndexCreateSuccess {
     acknowledged: bool,
     shards_acknowledged: bool,
@@ -44,30 +43,46 @@ pub async fn create_index_req(client: &EsClient, index: &str) -> Result<EsIndexC
 #[cfg(test)]
 mod tests {
     use super::{EsIndexCreateSuccess, create_index_req};
-    use crate::client::{EsClient, Version};
+    use crate::client::EsClient;
     use mockito::mock;
     use tokio::runtime::Runtime;
 
     #[test]
     fn successfully_create_index() {
         let mut rt = Runtime::new().unwrap();
-        let _es_mock = mock("PUT", "/test")
+        let _client_mock = mock("GET", "/")
             .with_status(200)
             .with_header("content-type", "application/json")
             .with_body(r#"{
-                "acknowledged": "true",
-                "shards_acknowledged": "true",
+                "name": "DbU-kT2",
+                "cluster_name": "docker-cluster",
+                "cluster_uuid": "HjwlCaVKQo2766zcX_l7DQ",
+                "version": { 
+                    "number": "6.8.6",
+                    "build_flavor": "default",
+                    "build_type": "docker",
+                    "build_hash": "3d9f765",
+                    "build_date": "2019-12-13T17:11:52.013738Z",
+                    "build_snapshot": false,
+                    "lucene_version": "7.7.2",
+                    "minimum_wire_compatibility_version": "5.6.0",
+                    "minimum_index_compatibility_version": "5.0.0"
+                },
+                "tagline": "You Know, for Search" 
+            }"#)
+            .create();
+
+        let _index_mock = mock("PUT", "/test")
+            .with_status(200)
+            .with_header("content-type", "application/json")
+            .with_body(r#"{
+                "acknowledged": true,
+                "shards_acknowledged": true,
                 "index": "test"
             }"#)
             .create();
 
-        let client = EsClient {
-            host: "http://127.0.0.1".to_owned(), 
-            port: 1234.to_string(),
-            client: reqwest::Client::new(),
-            version: Version::Es6,
-        };
-
+        let client = EsClient::new("http://127.0.0.1", 1234);
         let res = create_index_req(&client, "test");
         let res = rt.block_on(res);
         let expected_res = EsIndexCreateSuccess {
@@ -75,6 +90,6 @@ mod tests {
             shards_acknowledged: true,
             index: "test".to_owned(),
         };
-        assert_eq!(res, expected_res);
+        assert_eq!(res.unwrap(), expected_res);
     }
 }
