721f75bd24e95fec295bfbb70cc2b07ecc7a3ac7
Benjamin Slater <ben@hackcapital.com>
2020-01-29 22:59:59 -0800

    created new doc module

---
+++ src/doc.rs
@@ -0,0 +1,67 @@
+use reqwest;
+use reqwest::StatusCode;
+
+use serde::{ Deserialize, Serialize };
+use crate::client::EsClient;
+use crate::utils::serialize_response;
+
+#[derive(Deserialize, Debug, PartialEq)]
+pub struct EsIndexDocResponse {
+    #[serde(rename = "_shards")]
+    shards: ShardResults,
+    #[serde(rename = "_index")]
+    index: String,
+    #[serde(rename = "_type")]
+    doc_type: String,
+    #[serde(rename = "_id")]
+    id: String,
+    #[serde(rename = "_version")]
+    version: u32,
+    #[serde(rename = "_seq_no")]
+    seq_no: u32,
+    #[serde(rename = "_primary_term")]
+    primary_term: u32,
+    result: String,
+}
+
+#[derive(Deserialize, Debug, PartialEq)]
+struct ShardResults {
+    total: u16,
+    successful: u16,
+    failed: u16,
+}
+
+pub async fn index_doc_req<T: Serialize>(
+    client: &EsClient,
+    index: &str,
+    doc_type: Option<&str>,
+    id: Option<&str>,
+    operation: Option<&str>,
+    data: T
+    ) -> Result<EsIndexDocResponse, Box<dyn std::error::Error>> 
+{
+    let res = if let Some(id) = id {
+        client.put_doc(index, doc_type, id, operation)
+            .json(&data)
+            .send()
+            .await?
+    } else {
+        client.post(index, doc_type, None)
+            .json(&data)
+            .send()
+            .await?
+    };
+
+    let res = match res.status() {
+        StatusCode::OK => {
+            let text = res.text().await?;
+            let data = serialize_response::<EsIndexDocResponse>(&text)?;
+            data
+        },
+        //StatusCode::BAD_REQUEST => {
+        //},
+        _ => panic!("Request failed in an unexpected way..."),
+    };
+    Ok(res)
+}
+
--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,5 @@
 pub mod client;
+pub mod doc;
 pub mod errors;
 pub mod index;
 pub mod info;
