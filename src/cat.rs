db34d6777955d7e12fcde53eac5043d5ad5a8bf7
Benjamin Slater <benjamin.ed.slater@gmail.com>
2020-04-06 09:01:07 -0700

    added tests for aliases

--- src/cat.rs
+++ src/cat.rs
@@ -2,12 +2,12 @@ use reqwest;
 use reqwest::StatusCode;
 use crate::client::EsClient;
 
-#[derive(Debug)]
+#[derive(Debug, PartialEq)]
 pub struct AliasResponse {
     aliases: Vec<AliasResults>,
 }
 
-#[derive(Debug)]
+#[derive(Debug, PartialEq)]
 pub struct AliasResults {
     alias: String,
     index: String,
@@ -51,4 +51,108 @@ pub async fn aliases_req(client: &EsClient) -> Result<AliasResponse, Box<dyn std
     };
 
     Ok(res)
+}
+
+#[cfg(test)]
+mod tests {
+    use super::{
+        aliases_req,
+        AliasResponse,
+        AliasResults,
+    };
+    use crate::client::EsClient;
+
+    use mockito::mock;
+    use tokio::runtime::Runtime;
+    use serde_json::json;
+    use serde::Deserialize;
+
+    #[test]
+    fn successful_aliases_es6_no_results() {
+        let mut rt = Runtime::new().unwrap();
+        let _client_mock = mock("GET", "/")
+            .with_status(200)
+            .with_header("content-type", "application/json")
+            .with_body(r#"{
+                "name": "DbU-kT2",
+                "cluster_name": "docker-cluster",
+                "cluster_uuid": "HjwlCaVKQo2766zcX_l7DQ",
+                "version": { 
+                    "number": "6.8.6",
+                    "build_flavor": "default",
+                    "build_type": "docker",
+                    "build_hash": "3d9f765",
+                    "build_date": "2019-12-13T17:11:52.013738Z",
+                    "build_snapshot": false,
+                    "lucene_version": "7.7.2",
+                    "minimum_wire_compatibility_version": "5.6.0",
+                    "minimum_index_compatibility_version": "5.0.0"
+                },
+                "tagline": "You Know, for Search" 
+            }"#)
+            .create();
+
+        let _aliases_mock = mock("GET", "/_cat/aliases")
+            .with_status(200)
+            .with_body("")
+            .create();
+
+        let client = EsClient::new("http://127.0.0.1", 1234);
+        let res = aliases_req(&client);
+        let res = rt.block_on(res);
+
+        let expected_res = AliasResponse {
+            aliases: Vec::new()
+        };
+        assert_eq!(res.unwrap(), expected_res);
+    }
+
+    #[test]
+    fn successful_aliases_es6_with_results() {
+        let mut rt = Runtime::new().unwrap();
+        let _client_mock = mock("GET", "/")
+            .with_status(200)
+            .with_header("content-type", "application/json")
+            .with_body(r#"{
+                "name": "DbU-kT2",
+                "cluster_name": "docker-cluster",
+                "cluster_uuid": "HjwlCaVKQo2766zcX_l7DQ",
+                "version": { 
+                    "number": "6.8.6",
+                    "build_flavor": "default",
+                    "build_type": "docker",
+                    "build_hash": "3d9f765",
+                    "build_date": "2019-12-13T17:11:52.013738Z",
+                    "build_snapshot": false,
+                    "lucene_version": "7.7.2",
+                    "minimum_wire_compatibility_version": "5.6.0",
+                    "minimum_index_compatibility_version": "5.0.0"
+                },
+                "tagline": "You Know, for Search" 
+            }"#)
+            .create();
+
+        // Formatting is based on how reqwest resolves the non json body ES sends back.
+        let _aliases_mock = mock("GET", "/_cat/aliases")
+            .with_status(200)
+            .with_body("test_alias test - - -\n")
+            .create();
+
+        let client = EsClient::new("http://127.0.0.1", 1234);
+        let res = aliases_req(&client);
+        let res = rt.block_on(res);
+
+        let expected_res = AliasResponse {
+            aliases: vec![
+                AliasResults {
+                    alias: "test_alias".to_owned(),
+                    index: "test".to_owned(),
+                    filter: "-".to_owned(),
+                    routing_index: "-".to_owned(),
+                    routing_search: "-".to_owned(),
+                }
+            ]
+        };
+        assert_eq!(res.unwrap(), expected_res);
+    }
 }
